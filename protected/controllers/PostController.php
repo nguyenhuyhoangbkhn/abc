08d450315011253e7a789ba05e2293fb6160b084
nguyenhuyhoangbkhn@gmail.com <nguyenhuyhoangbkhn@gmail.com>
2015-10-22 12:07:46 +0700

    fix them img

--- protected/controllers/PostController.php
+++ protected/controllers/PostController.php
@@ -27,17 +27,12 @@ class PostController extends Controller
 	public function accessRules()
 	{
 		return array(
-			array('allow',  // allow all users to perform 'index' and 'view' actions
+			array('allow',  // allow all users to access 'index' and 'view' actions.
 				'actions'=>array('index','view','search'),
 				'users'=>array('*'),
 			),
-			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update'),
+			array('allow', // allow authenticated users to access all actions
 				'users'=>array('@'),
-			),
-			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
-				'users'=>array('admin'),
 			),
 			array('deny',  // deny all users
 				'users'=>array('*'),
